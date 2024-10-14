f820bb1d6336a34e5c27f58c367a92f5ad2f242f
AkhilVarma <91078213+akhilankathi@users.noreply.github.com>
2024-10-14 10:39:26 +0000

    added basic built in data structures in javascript in types_overview file

---
+++ src/data_structures/types_overview.js
@@ -0,0 +1,17 @@
+// ARRAY
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1];
+
+// OBJECT
+const obj = {
+  name: "akhil",
+  age: 27,
+};
+
+// SET
+const _set = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+//MAP
+const _map = new Map([
+  ["name", "akhil"],
+  ["age", 27],
+]);
